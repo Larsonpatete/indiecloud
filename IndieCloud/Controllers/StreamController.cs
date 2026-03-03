aa4a203782c9bb11387220cc0a92e2e5e974750a
Larson Patete <larsonpatete@hotmail.com>
2026-03-02 19:27:47 -0600

    initial commit

---
+++ IndieCloud/Controllers/StreamController.cs
@@ -0,0 +1,16 @@
+﻿using IndieCloud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IndieCloud.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StreamController : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateStream([FromBody] Message message)
+    {
+
+    }
+}
---
+++ IndieCloud/Models/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace IndieCloud.Models;
+
+public class Message
+{
+    public required string Chat { get; set; }
+    public string Device { get; set; }
+    public DateTime TimeStamp { get; set; }
+}
